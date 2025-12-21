9903476797afbb319ac4085b742b748c0ba8cbb3
roman <johndoe@email.com>
2025-12-21 16:02:40 +0300

    1

--- js/kb_parser.js
+++ js/kb_parser.js
@@ -17,13 +17,13 @@ function kbparse(new_kills) {
 				if (system == "sys_30003522") {
 					console.log("new_kills", new_kills);
 					console.log("old_kills", old_kills);
-					console.log(system, Object.keys(old_kills)[8]);
-					console.log(typeof (system), typeof (Object.keys(old_kills)[8]));
-					console.log(system.length,Object.keys(old_kills)[8].length);
-					console.log(system == Object.keys(old_kills)[8], system === Object.keys(old_kills)[8]);
-					console.log(old_kills[Object.keys(old_kills)[8]]);
+					console.log(system, Object.keys(old_kills)[38]);
+					console.log(typeof (system), typeof (Object.keys(old_kills)[38]));
+					console.log(system.length,Object.keys(old_kills)[38].length);
+					console.log(system == Object.keys(old_kills)[38], system === Object.keys(old_kills)[38]);
+					console.log(old_kills[Object.keys(old_kills)[38]]);
 					console.log([...system].map(c => c.charCodeAt(0)));
-					console.log([...Object.keys(old_kills)[8]].map(c => c.charCodeAt(0)));
+					console.log([...Object.keys(old_kills)[38]].map(c => c.charCodeAt(0)));
 					const cleanSystem = system.normalize("NFKC").replace(/[^\x20-\x7E]/g, "");
 					console.log(old_kills[cleanSystem]);
 				}
