cdd28b76097f0a72ba40f4c503ce7c9b4c04bbf5
stuffcreator1337 <bryakas@gmail.com>
2026-02-05 13:10:41 +0300

    tooltips separate file added

--- js/tooltip.js
+++ js/tooltip.js
@@ -216,21 +216,3 @@
   });
 
 }).call(this);
-
-var edgetooltip = document.getElementById('edge-tooltip');
-
-function showEdgeTooltip(text, x, y) {
-    edgetooltip.textContent = text;
-    edgetooltip.style.left = (x + 12) + 'px';
-    edgetooltip.style.top  = (y + 12) + 'px';
-    edgetooltip.style.opacity = 1;
-}
-
-function moveEdgeTooltip(x, y) {
-    edgetooltip.style.left = (x + 12) + 'px';
-    edgetooltip.style.top  = (y + 12) + 'px';
-}
-
-function hideEdgeTooltip() {
-    edgetooltip.style.opacity = 0;
-}
